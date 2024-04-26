8280e3c48bd68fc960c7051d1a61e2c12fd32422
saimalam843 <saimalam843@gmail.com>
2024-04-26 21:46:17 +0500

    Jest Tests and Linting finalized

--- __tests__/iridium.test.js
+++ __tests__/iridium.test.js
@@ -1,7 +1,7 @@
 //const iridium = require('../src/iridium');
 
 describe('Iridium Module Tests', () => {
-  // Test Case 1: Placeholder test to ensure Jest setup is working
+  //Placeholder test to ensure Jest setup is working
   test('Placeholder test', () => {
     expect(true).toBe(true);
   });
--- __tests__/satellite.test.js
+++ __tests__/satellite.test.js
@@ -1,7 +1,7 @@
 //const satellite = require('../src/satellite');
 
 describe('Satellite Module Tests', () => {
-  // Test Case 1: Placeholder test to ensure Jest setup is working
+  // Placeholder test to ensure Jest setup is working
   test('Placeholder test', () => {
     expect(true).toBe(true);
   });
